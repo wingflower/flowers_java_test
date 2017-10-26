1546c504a8b82e62e7f5bb4db84bbd65522e303e
김범진A <k7720202@douzone.com>
2017-10-26 22:45:23 +0900

    영어로 수정 및 코드 정리

--- src/kbj/SenderImpl.java
+++ src/kbj/SenderImpl.java
@@ -1,122 +1,206 @@
 package kbj;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SenderImpl {
-
-	public static void main(String[] args) {
-		// 키보드 입력 받기 위한 변수
-		// Scanner scanner = new Scanner(System.in);
-		// System.out.println("보낼 메세지 입력 :");
-		// String msg = scanner.next();
-
-		String msg = "SenderID::Contents";
+	
+	public static void main(String[] args) throws Exception {
 		
+		String clientIP = Inet4Address.getLocalHost().getHostAddress();
+		String mgmtIp = "10.101.30.176";
+		int mgmtPort = 30000;
+		String msg = "[meta]cpu,mem," + clientIP + ":" + mgmtPort;
 
+		// it gets a collector ID and sender ID from the mgmt		
+		Map<String, String> ids = getIds(msg, mgmtIp, mgmtPort);
+		
+//		String collectorId = ids.get("collectorId"); 
+		String senderId = ids.get("senderId");
+		
+		String[] collectorAddress = ids.get("collectorIp").split(":");
+		
+		String collectorIp = collectorAddress[0];
+		int collectorPort = Integer.parseInt(collectorAddress[1].trim());
+				
+		// it keep checking the connection till it connects to the collector
+		checkConnection(collectorIp, collectorPort, senderId);
+		
+		// it starts to send logs
+		sendLog(senderId, collectorIp, collectorPort);
+	}
+
+	/**
+	 * It gets a collector id and a sender id from mgmt
+	 * @author Bum-jin Kim
+	 * @param msg
+	 * @return
+	 */
+	@SuppressWarnings("resource")
+	public static Map<String, String> getIds(String msg, String mgmtIp, int mgmtPort) {
 		try {
-			// 전송할 수 있는 UDP 소켓 생성
+			// it creates a udp socket
 			DatagramSocket dsoc = new DatagramSocket();
-			System.out.println("UDP 소켓 생성");
+			System.out.println("UDP socket's been generated.");
+			
+			// it creates the mgmt ip address
+			InetAddress ia = InetAddress.getByName(mgmtIp);
+			System.out.println(ia.getHostAddress() + " address's been generated.");
 
-			// 받을 곳의 주소 생성
-			InetAddress ia = InetAddress.getByName("192.168.1.8");
-			System.out.println(ia.getHostAddress() + " 주소 생성 ");
-
-			// 전소할 데이터 생성
-			DatagramPacket dp = new DatagramPacket(msg.getBytes(), msg.getBytes().length, ia, 9999);
-			byte[] data = dp.getData();
-
-			// epdlxj wjsthd
-			dsoc.send(dp);
-			System.out.println("데이터 보냄 ");
-
+			// it sends the msg to the mgmt
+			sendPacket(ia, msg, dsoc, mgmtPort);
+			
 			/**********************************
-			 * ///////////////수신/////////////////
+			 * Wait for data
 			 **********************************/
+			System.out.println("waitong for data from the mgmt");
 			while (true) {
-				byte[] buffer = new byte[msg.getBytes().length];
-				// 반송되는 DatagramPacket을 받기 위해 receivePacket 생성한 후 대기
+//				sendPacket(ia, msg, dsoc);
+				
+//				byte[] buffer = new byte[msg.getBytes().length];
+				byte[] buffer = new byte[1024];
+
+				// it receives data
 				DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
 				dsoc.receive(receivePacket);
 
-				// 받은 결과 출력
+				// it prints the received data
 				String receviedMsg = new String(receivePacket.getData(), 0, receivePacket.getData().length);
-				System.out.println("전송받은 문자열 : " + receviedMsg);
+				System.out.println("The received data : " + receviedMsg);
 				
-				if(receviedMsg.length() != 0) {
-					break;
+				
+				if(receviedMsg.isEmpty() == false) {
+					System.out.println("The mgmt's been connected ");
+					
+					String[] splitReceivedMsg = receviedMsg.split(",");
+					Map<String, String> result = new HashMap<>();
+					result.put("collectorId", splitReceivedMsg[0]);
+					result.put("senderId", splitReceivedMsg[1]);
+					result.put("collectorIp", splitReceivedMsg[2]);
+					
+					dsoc.close();
+					return result;
 				}
+				
+				Thread.sleep(500);
 			}
-
-			dsoc.close();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+		return null;
+	}
+	
+	/**
+	 * It keep checking the connection till it connects to the collector
+	 * @author Bum-jin Kim
+	 * @param ip
+	 * @param port
+	 * @throws Exception
+	 */
+	public static void checkConnection(String ip, int port, String senderId) throws Exception {
+		InetAddress ia = InetAddress.getByName(ip);
+		DatagramSocket dsoc = new DatagramSocket();
+		
+		DatagramPacket dp = new DatagramPacket(senderId.getBytes(), senderId.getBytes().length, ia, port);
+		
+		while(true) {
+			dsoc.send(dp);
+			
+			byte[] buffer = new byte[1024];
+			// 반송되는 DatagramPacket을 받기 위해 receivePacket 생성한 후 대기
+			DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+			dsoc.receive(receivePacket);
+
+			// 받은 결과 출력
+			String receviedMsg = new String(receivePacket.getData(), 0, receivePacket.getData().length);
+			System.out.println("전송받은 문자열 : " + receviedMsg);
+			
+			
+			if(receviedMsg.isEmpty() == false) {
+				System.out.println("collector 연결 확인 ");
+				
+				dsoc.close();
+				break;
+			}
+			
+			Thread.sleep(500);
+		}
+	}
+	
+	/**
+	 * It sends logs to the ip
+	 * @author 김범진A
+	 * @param id
+	 * @param ip
+	 * @param port
+	 * @throws Exception
+	 */
+	public static void sendLog(String id, String ip, int port) throws Exception {
+		
+		InetAddress ia = InetAddress.getByName(ip);
+		
+		String clientIP = Inet4Address.getLocalHost().getHostAddress();
+		DatagramSocket dsoc = new DatagramSocket();
+		
+		String msg = id + "," + clientIP + ":" + port + ",name|work";
+		String[] name = {"KBJ", "SSY", "KDH"};
+		String[] work = {"Sender", "Collector", "WatchDog"};
+		
+		
+		DatagramPacket dp = new DatagramPacket(msg.getBytes(), msg.getBytes().length, ia, port);
+		dsoc.send(dp);
+		
+		while(true) {
+			int ran = randomRange(0, 2);
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			String date = sdf.format(new Date());
+			
+			msg = String.format("[LOG %s]%s,%s", date, name[ran], work[ran]);
+			dp = new DatagramPacket(msg.getBytes(), msg.getBytes().length, ia, port);
+			dsoc.send(dp);
+			Thread.sleep((ran+1) * 1000);
+		}
+		
+	}
+	
+	/**
+	 * Send the msg to the ia
+	 * @author Bum-jin Kim
+	 * @param ia
+	 * @param msg
+	 * @param dsoc
+	 * @throws IOException
+	 */
+	public static void sendPacket(InetAddress ia, String msg, DatagramSocket dsoc, int port) throws IOException {
+		DatagramPacket dp = new DatagramPacket(msg.getBytes(), msg.getBytes().length, ia, port);
+		dsoc.send(dp);
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("[ ");
+		sb.append(msg);
+		sb.append(" ]");
+		sb.append(" was sent to ");
+		sb.append(ia.getHostAddress());
+		
+		System.out.println(sb.toString());
 	}
 
-	// public static void main(String[] args) {
-	//
-	// String ip = "192.168.1.8";
-	// int port = 9999;
-	//
-	// InetAddress inetaddr = null;
-	//
-	// try {
-	// // DatagramPacket에 들어갈 ip 주소가 InetAddress 형태여야 함
-	// inetaddr = InetAddress.getByName(ip);
-	// } catch (UnknownHostException e) {
-	// System.out.println("잘못된 도메인이나 ip입니다.");
-	// System.exit(1);
-	// }
-	//
-	// /**********************************
-	// * ///////////////전송/////////////////
-	// **********************************/
-	// DatagramSocket dsock = null;
-	//
-	// try {
-	// // 키보드로부터 서버에게 전송할 문자열을 입력받기 위해
-	// // System.in을 BufferedReader 형태로 변환
-	// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	// dsock = new DatagramSocket();
-	//
-	// String line = "SenderId::Contents";
-	// while ((line = br.readLine()) != null) {
-	//
-	// // DatagramPacket에 각 정보를 담고 전송
-	//
-	// DatagramPacket sendPacket = new DatagramPacket(line.getBytes(),
-	// line.getBytes().length, inetaddr, port);
-	// dsock.send(sendPacket);
-	//
-	//// if (line.equals("quit"))
-	//// break;
-	//
-	// /**********************************
-	// * ///////////////수신/////////////////
-	// **********************************/
-	// byte[] buffer = new byte[line.getBytes().length];
-	// // 반송되는 DatagramPacket을 받기 위해
-	// // receivePacket 생성한 후 대기
-	// DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
-	// dsock.receive(receivePacket);
-	//
-	// // 받은 결과 출력
-	// String msg = new String(receivePacket.getData(), 0,
-	// receivePacket.getData().length);
-	// System.out.println("전송받은 문자열 : " + msg);
-	//
-	// }
-	// System.out.println("UDPEchoClient를 종료합니다.");
-	// } catch (Exception ex) {
-	// System.out.println(ex);
-	// } finally {
-	// if (dsock != null)
-	// dsock.close();
-	// }
-	// }
+	/**
+	 * It generates a random number from n1 to n2
+	 * @author Bum-jin Kim
+	 * @param n1
+	 * @param n2
+	 * @return
+	 */
+	public static int randomRange(int n1, int n2) {
+		return (int) (Math.random() * (n2 - n1 + 1)) + n1;
+	}
+
 }
